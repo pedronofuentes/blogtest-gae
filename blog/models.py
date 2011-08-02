bae065f065a2891ec3f2d8772f0cf947586ed0c7
Pedro <pedro.nofuentes@gmail.com>
2011-08-02 18:56:14 +0200

    added Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
+# -*- coding:utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=255)
+  slug = models.SlugField(510, blank=True)
+  body = models.TextField()
+  author = models.ForeignKey(User)
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
