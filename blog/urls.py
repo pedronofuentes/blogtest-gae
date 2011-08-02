97b59668353281097ce895bd30de4f6b19308ab2
Pedro <pedro.nofuentes@gmail.com>
2011-08-02 22:52:21 +0200

    created get_post view instead object_detail

--- blog/urls.py
+++ blog/urls.py
@@ -3,14 +3,9 @@ from django.conf.urls.defaults import *
 from django.views.generic import list_detail
 from blog.models import Post
 
-urlpatterns = patterns('',
+urlpatterns = patterns('blog.views',
   ('^$', list_detail.object_list, {'queryset': Post.objects.order_by('-created'),
                                     'template_name': 'post_list.html',
                                     'template_object_name': 'post'}),
-  ('^post/(?P<slug>.+)', list_detail.object_detail, {
-                                    'queryset': Post.objects.all(),
-                                    'slug_field': 'slug',
-                                    'template_name': 'post_detail.html',
-                                    'template_object_name': 'post'
-  }),
+  ('^post/(?P<slug>.+)', 'get_post'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+# -*- coding:utf-8 -*-
+from django.http import HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+from blog.models import Post
+from blog.forms import PostForm
+
+def get_post(request, slug):
+  post = get_object_or_404(Post,slug=slug)
+  return render_to_response('post_detail.html', {'post': post}, RequestContext(request))
