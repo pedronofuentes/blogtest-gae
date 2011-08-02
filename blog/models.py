e9ca08bae9eb642372d4e7c36f005042cb8ef452
Pedro <pedro.nofuentes@gmail.com>
2011-08-02 19:13:07 +0200

    added slug behaviour: before save it slugifies the post title

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 class Post(models.Model):
   title = models.CharField(max_length=255)
-  slug = models.SlugField(510, blank=True)
+  slug = models.SlugField(max_length=510, blank=True)
   body = models.TextField()
   author = models.ForeignKey(User)
   created = models.DateTimeField(auto_now_add=True)
@@ -12,3 +13,7 @@ class Post(models.Model):
   
   def __unicode__(self):
     return u'%s' % self.title
+  
+  def save(self, *args, **kwargs):
+    self.slug = slugify(self.title)
+    super(Post,self).save(*args, **kwargs)
