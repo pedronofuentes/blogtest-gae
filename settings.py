497f0d19814cb40d8d89a31abba7dcd516a7f2d8
Pedro <pedro.nofuentes@gmail.com>
2011-08-02 19:08:22 +0200

    added blog to installed apps

--- settings.py
+++ settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = (
     'autoload',
     'dbindexer',
     'django.contrib.admin',
+    'blog',
 
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
