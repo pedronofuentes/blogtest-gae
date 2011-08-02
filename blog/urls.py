bea3f1c7e1d06dcd2262927ff1845692ea1c880c
Pedro <pedro.nofuentes@gmail.com>
2011-08-02 20:21:54 +0200

    added post_list view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 # -*- coding:utf-8 -*-
 from django.conf.urls.defaults import *
+from django.views.generic import list_detail
+from blog.models import Post
 
 urlpatterns = patterns('',
-  ('^$', 'django.views.generic.simple.direct_to_template',
-   {'template': 'home.html'}),
+  ('^$', list_detail.object_list, {'queryset': Post.objects.order_by('-created'),
+                                    'template_name': 'post_list.html',
+                                    'template_object_name': 'post'}),
 )
